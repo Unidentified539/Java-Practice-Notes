7125f58559cb129bd19fac782e9541e385904a2e
Itachi Uchiha <71221226+Unidentified539@users.noreply.github.com>
2021-02-14 17:26:34 -0600

    Create interateAnArray.java

---
+++ interateAnArray.java
@@ -0,0 +1,12 @@
+public class interateAnArray {
+    public static void main(String[] args) {
+        int[] oneThrough10 = {1,2,3,4,5,6,7,8,9,10};
+        int i,x;
+
+        for (i = 0; i < oneThrough10.length; i++) {
+            x = oneThrough10[i];
+            System.out.println(x);
+        }
+
+    }
+}
