8c5b3494e342bdded58282ac989327828bd38843
Itachi Uchiha <71221226+Unidentified539@users.noreply.github.com>
2021-02-15 16:16:52 -0600

    Create newArray.java

---
+++ Arrays/newArray.java
@@ -0,0 +1,12 @@
+public class newArray {
+    public static void main(String[] args) {
+       //  Datatype Of array     name of the array            DATA TO PUT IN THE ARRAY
+           String[]              nameGoesHere         =       {"Data goes here" };
+           
+           
+           // Keep in mind this is not the cleanest way to write an array it just so you can understand the parts
+        // Correct way to write a array
+        
+        String[] nameOfCorrectArr = {" Data goes here"};
+    }
+}
