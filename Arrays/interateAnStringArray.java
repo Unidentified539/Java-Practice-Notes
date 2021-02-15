010c28bb714cbe8432c0a9d2acac94c4de597749
Itachi Uchiha <71221226+Unidentified539@users.noreply.github.com>
2021-02-15 15:50:21 -0600

    Create interateAnStringArray.java

---
+++ Arrays/interateAnStringArray.java
@@ -0,0 +1,12 @@
+public class interateAnStringArray {
+    public static void main(String[] args) {
+        String[] classes = {"Economy", "Premium Economy", "Business", "First Class"};
+        String x;
+        int i;
+        for (i = 0; i < classes.length; i++) {
+            x = classes[i];
+            System.out.println(x);
+        }
+
+    }
+}
