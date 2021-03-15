483f412f747b8035638f18739ee2690153fb45f8
dev <Solomonaziel9@gmail.com>
2021-03-15 17:41:41 -0500

    More methods

--- Files/Person/practiceFiles/userIp.java
+++ Files/Person/practiceFiles/userIp.java
@@ -4,18 +4,11 @@ import org.json.*;
 import methods.mainPackage;
 
 public class userIp {
-    public String name;
 
-    public userIp(String name) {
-        this.name = name;
-    }
     public static void main(String[] args) throws Exception {
         mainPackage allFunctions = new mainPackage();
         String jsonFile = allFunctions.searchIPAddress("68.46.83.189");
-        System.out.println(jsonFile);
-        JSONObject obj = new JSONObject(jsonFile);
-        System.out.println(obj.getString("city"));
-        System.out.println(allFunctions.("10.0.0.32"));
+
     }
 
 }
